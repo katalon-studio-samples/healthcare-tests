527be6149e844cb220f287321873a6853f07c7f9
Alex <dev.alex.88@gmail.com>
2021-07-08 14:33:53 +0700

    update

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>50b958e4-e440-434a-a17d-47bc9ddb1d01</testSuiteGuid>
+   <testCaseLink>
+      <guid>46a6fa48-f592-475d-afa3-3fb439f89d95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Hit me</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8806f96-e24a-4f88-b96e-e32bdfd5cf6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Select item</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d3cd474-c546-43f7-b78a-4c15248e1b0f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Hit me with scroll</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54e1464f-fb94-4ff5-806f-644361d72e16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Hover me</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fc7d5c54-3c86-480d-aa25-1d1fc3243c61</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Fill form</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
